0fff6a1f87974bfdd7229f3e869a37913176f0a9
Umay Eras <umayeras@hotmail.com>
2020-05-13 20:30:56 +0300

    Initial Commit
    Project created. Layers added.

---
+++ WebApp/WebApp/Controllers/SampleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SampleController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok("Ok");
+        }
+    }
+}
