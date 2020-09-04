7b0391f44f7ddc13ebe8b5b93ba361eda5a248c9
Umay Eras <umayeras@hotmail.com>
2020-09-04 16:15:40 +0300

    Added Swagger

--- WebApp/WebApp/Startup.cs
+++ WebApp/WebApp/Startup.cs
@@ -1,8 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace WebApp
 {
@@ -14,12 +16,38 @@ namespace WebApp
         }
 
         private IConfiguration Configuration { get; }
-        
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            ConfigureSwagger(services);
         }
-        
+
+        private static void ConfigureSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "WebApi Boilerplate",
+                    Description = "A boilerplate ASP.NET Core Web API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Umay ERAS",
+                        Email = "umayeras@hotmail.com",
+                        Url = new Uri("http://umayeras.com"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under MIT",
+                        Url = new Uri("https://github.com/umayeras/webapi-boilerplate/blob/master/LICENSE"),
+                    }
+                });
+            });
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,12 +55,12 @@ namespace WebApp
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API Boilerplate V1"); });
+
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
